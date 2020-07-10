32cd32c2b5959b9d4e991c75734791cd4c36e30e
suganya <suganya.suganya10@gmail.com>
2020-07-10 16:12:05 +0100

    Removed Unwanted step definition feature file Corrected the locator in job_blog_steps.js

--- features/step_definitions/job_blog_steps.js
+++ features/step_definitions/job_blog_steps.js
@@ -7,8 +7,8 @@ defineSupportCode(function({When, Then}) {
 
     Then(/^I Verifying \("([^"]*)"\) blog Text\("([^"]*)"\) and Viewing article link\("([^"]*)"\) is working with (\d+)$/, function (job,jobText,link,index) {
         this.driver.executeScript("window.scrollBy(0,500)");
-        this.driver.findElement(By.css('section[class ="'+job+'brick"]')).isDisplayed();
-        let blog = this.driver.findElement(By.xpath('//*[@id="main"]/div[4]/div/div/div['+index+']/section/div/h2/span)'));
+        this.driver.findElement(By.css('section[class ="'+job+' brick"]')).isDisplayed();
+        let blog = this.driver.findElement(By.xpath('//*[@id="main"]/div[4]/div/div/div['+index+']/section/div/h2/span'));
         this.driver.executeScript('return arguments[0].innerHTML', blog).then(textValue => {
             assert.equal(jobText,textValue)
         });
